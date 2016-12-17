abf03a31bb8c1806b64a8adb3649aed9ab3f5f71
EmilyNuEn <emilyhistorybuff@gmail.com>
2016-12-17 17:49:50 -0500

    minor bug fix

--- level8.js
+++ level8.js
@@ -23,7 +23,7 @@ function checkForCompletion(){
 		var secondPoint = points[1];
 		var otherSegments = findSegmentsWithOneOf(firstPoint, secondPoint);
 		var possibleThirdPoint = intersectingTrianglesOnPointsOtherThan(otherSegments, firstPoint, secondPoint);
-		if(possibleThirdPoint != null && calculateDistance(possibleThirdPoint.x, possibleThirdPoint.y, firstPoint.x, firstPoint.y) != lines[0].length){
+		if(possibleThirdPoint != null && Math.abs(calculateDistance(possibleThirdPoint.x, possibleThirdPoint.y, firstPoint.x, firstPoint.y) - lines[0].length) > pointRadius){
 			nextLevelButtonHidden = false;
 		}
 	}
