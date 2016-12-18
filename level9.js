cdb0c6d2ed19597ab71674643aa724e3ee48d83f
FOOSPRJohnF <johnnf8@gmail.com>
2016-12-17 23:52:15 -0500

    Finished Level 9
    I was originally going to create a new midpoint tool, but still need to
    figure out how to do that with the new framework.

--- level9.js
+++ level9.js
@@ -1,84 +1,25 @@
-var points = [new Point(400, 400)];
-var lines = [new Segment(400,400,600,200), new Segment(400,400,600,450)]
-var circles = new Array(); 
-var midpoint = new Array();
-
-var finalPoints = new Array();
-var finalLines = new Array();
-var finalCircles = new Array();
-var finalMidpoint = new Array(); //Need to find out what the midpoint would be
-
+finalPoints = new Array();
+finalCircles = new Array();
+finalLines = [new Segment(400,400, 600, 325)];
 var levelNumber = 9;
-var lastMouseDown;
-var nextLevelButtonHidden = true;
-var secondClick = false;
-var pointRadius = 5;
-var errorLine = 10;
-
-var canvas = document.getElementById("mainContent");
-var ctx = canvas.getContext("2d");
-var toolbarState = document.getElementById("toolbar").getAttribute("state");
-var nextLevelButton = document.getElementById("nextLevelButton");
+/////////////////////////////////////////////////////////
+// To Common
 updateButton();
+fillLevelData();
 updateCanvas();
+
 console.log("source is " + document.getElementById("level").src);
 
 
-nextLevelButton.onclick = function(event){
-	$ ("#level").src = "level" + (levelNumber++) + ".js";
+function fillLevelData()
+{
+	levelNumber = 9;
+	lines = [new Segment(400,400,600,200), new Segment(400,400,600,450)];
+	points = [new Point(600,325)]
+	finalPoints = new Array();
+	finalCircles = new Array();
+	finalLines = [new Segment(400,400, 600, 325)];
 }
-
-
-canvas.onmousedown = function(event){
-	var x = event.clientX - event.offsetX;
-	var y = event.clientY - event.offsetY;
-	var toolbarState = document.getElementById("toolbar").getAttribute("state");
-	if(!closeToAnotherPoint(x, y)){
-		points.push(new Point(x, y));
-	}
-	if(toolbarState == "point"){
-		lastMouseDown = null;
-	}
-	if(toolbarState == "segment" || toolbarState == "circle"){
-		if(lastMouseDown != event && lastMouseDown != null){
-			if(toolbarState == "segment"){
-				lines.push(new Segment(lastMouseDown.clientX - canvas.offsetLeft, lastMouseDown.clientY - canvas.offsetTop, x, y));
-			}
-			if(toolbarState == "circle"){
-				circles.push(new Circle(lastMouseDown.clientX - canvas.offsetLeft, lastMouseDown.clientY - canvas.offsetTop, x, y));
-			}
-			lastMouseDown = null;
-		}else{
-			lastMouseDown = event;
-		}
-	}
-	updateCanvas();
-	checkForCompletion();
-	updateButton();
-};
-
-canvas.onmouseup = function(event){
-	var x = event.clientX - canvas.offsetLeft;
-	var y = event.clientY - canvas.offsetTop;
-	var toolbarState = document.getElementById("toolbar").getAttribute("state");
-	if(!closeToAnotherPoint(x, y)){
-		points.push(new Point(x, y));
-	}
-	if(toolbarState == "segment" || toolbarState == "circle"){
-		if(lastMouseDown != null && lastMouseDown.clientX != event.clientX && lastMouseDown.clientY != event.clientY){
-			if(toolbarState == "segment"){
-				lines.push(new Segment(lastMouseDown.clientX - canvas.offsetLeft, lastMouseDown.clientY - canvas.offsetTop, x, y));
-			}
-			if(toolbarState == "circle"){
-				circles.push(new Circle(lastMouseDown.clientX - canvas.offsetLeft, lastMouseDown.clientY - canvas.offsetTop, x, y));
-			}
-			lastMouseDown = null;
-		}
-	}
-	updateCanvas();
-	checkForCompletion();
-	updateButton();
-};
 
 function testLineFoundWithinError(){
 	lines = [new Segment(100, 100, 50, 200)];
@@ -96,16 +37,29 @@ function testLineFoundWithinError(){
 	lines = new Array();
 }
 
+function testLineFoundInverted(){
+	lines = [new Segment(50, 200, 100, 100)];
+	var lineFound = lineFoundInverted(finalLines[0]);
+	console.log("lineFoundInverted test with known inverted line result: " + lineFound);
+	lines = [new Segment(50, 200, 1, 1)];
+	lineFound = lineFoundInverted(finalLines[0]);
+	console.log("lineFoundInverted test with known non-member line result: " + lineFound);
+	lines = new Array();
+}
 
+function testCalculateDistance(){
+	var distance = calculateDistance(1, 1, 2, 1);
+	console.log("distance from (1,1) to (2, 1): " + distance);
+	distance = calculateDistance(1, 1, 1, 2);
+	console.log("distance from (1, 1) to (1, 2): " + distance);
+	distance = calculateDistance(0, 0, 3, 4);
+	console.log("distance from (0, 0) to (3, 4): " + distance);
+}
 
 function checkForCompletion(){
 	var circlesFound = allCirclesFound();
 	var pointsFound = allPointsFound();
 	var linesFound = allLinesFound();
-	console.log("Circles " + circlesFound);
-	console.log("Points " + pointsFound);
-	console.log("lines: " + linesFound);
-
 	if(circlesFound && pointsFound && linesFound){
 		nextLevelButtonHidden = false;
 	}
@@ -195,7 +149,7 @@ function lineFoundWithinError(objectiveLine){
 		y2 = lines[i].y2;
 		point1DistanceOff = calculateDistance(objectiveX1, objectiveY1, x1, y1);
 		point2DistanceOff = calculateDistance(objectiveX2, objectiveY2, x2, y2);
-		if(point1DistanceOff <= errorLine && point2DistanceOff <= errorLine){
+		if(point1DistanceOff <= pointRadius && point2DistanceOff <= pointRadius){
 			console.log("Compatible line found for (" + objectiveX1 + ", " + objectiveX2 + ") to (" + objectiveX2 + ", " + objectiveY2 + ")");
 			return true;
 		}
@@ -219,110 +173,3 @@ function updateButton(){
 		$ ("#nextLevelButton").hide();
 	}
 }
-
-/*creates a point construct, contains the x and y variables*/
-function Point(x, y){
-	this.x = x;
-	this.y = y;
-}
-
-/*creates a segment out of two points, stores an x1, y1, x2, and a y2*/
-function Segment(x1, y1, x2, y2){
-	this.x1 = x1;
-	this.y1 = y1;
-	this.x2 = x2;
-	this.y2 = y2;
-}
-
-/*represents a circle defined by a center point and a radius defined with another point. Both points are in the constructor for simplicity*/
-function Circle(xCenter, yCenter, xOther, yOther){
-	this.radius = Math.sqrt(Math.pow((xCenter - xOther), 2) + Math.pow((yCenter - yOther),2));
-	this.xCenter = xCenter;
-	this.yCenter = yCenter
-}
-
-function Midpoint(x1,y1,x2, y2){
-	console.log("Need to work on this.....")
-}
-
-
-/*calculates the slope of a line segment*/
-function calculateSlope(segment){
-	var slope = (segment.b.y - segment.a.y)/(segment.b.x - segment.a.x);
-	return slope;
-}
-
-/*draws the points designated in the array points*/
-function drawPoints(context){
-	console.log("drwaing points");
-	for (i = 0; i < points.length; i++){
-		context.beginPath();
-		var x = points[i].x;
-		var y = points[i].y;
-		console.log("drawing point at x: " + x + " y: " + y);
-		context.fillStyle = "#0000ff";
-		context.arc(x, y, pointRadius, 0, 2*Math.PI);
-		context.fill();
-		context.stroke();
-	}
-}
-
-//Line 15 has the reference to context that I am looking for
-////////This is a new function!!!!!!!!!!//////////
-function drawMidpoint(context){
-	console.log("drawing a midpoint");
-	for(i=0; i< finalMidpoint.length; i++){
-		context.beginPath();
-		var x = (midpoint[i].x2 / midpoint[i].x1);
-		var y = (midpoint[i].y2 / midpoint[i].y1);
-		context.fillStyle = "#0000ff";
-		context.arc(x, y, pointRadius, 0, 2*Math.PI);
-		context.fill();
-		context.stroke();
-	}
-}
-
-/*draws the lines stored in the lines array*/
-function drawLines(context){
-	console.log("drawing lines");
-	for(i = 0; i < lines.length; i++){
-		context.beginPath();
-		context.moveTo(lines[i].x1, lines[i].y1);
-		context.lineTo(lines[i].x2, lines[i].y2);
-		context.stroke();
-	}
-}
-
-/* draws all the circles stored in the circles array*/
-function drawCircle(context){
-	console.log("drawing circles");
-	for(i = 0; i < circles.length; i++){
-		context.beginPath();
-		var x = circles[i].xCenter;
-		var y = circles[i].yCenter;
-		var radius = circles[i].radius;
-		console.log("drawing a circle at (" + x + ", " + y + ") with radius" + radius);
-		context.arc(x, y, radius, 0, 2*Math.PI);
-		context.stroke();
-	}
-}
-
-
-function closeToAnotherPoint(x, y){
-	var close = false;
-	for (var i = points.length - 1; i >= 0; i--) {
-		if(Math.sqrt(Math.pow((x - points[i].x), 2) - Math.pow((y - points[i].y), 2)) < pointRadius*2) {
-			close = true;
-		}
-	}
-	return close;
-}
-
-function updateCanvas(){
-	var canvas = document.getElementById("mainContent");
-	var context = canvas.getContext("2d");
-	drawPoints(context);
-	drawLines(context);
-	drawCircle(context);
-}
-
